935b484bf23ee2f6610d6ff8746c276cc533e6cb
Andrian <ayang@ebi.ac.uk>
2023-12-05 11:30:33 +0000

    Updated puzzle 2 solution to be more space efficient, though still too slow

--- puzzle_5/solution_2.py
+++ puzzle_5/solution_2.py
@@ -1,30 +1,35 @@
 import re
 
-def remap_nums(number_list, mapping):
-    new_nums = []
-    for num in number_list:
+def remap_nums(num_range, mappings):
+    min_num = float("inf")
+    for num in range(*num_range):
         new_num = num
-        for maps in mapping:
-            dest_start, source_start, range_val = maps
-            if num >= source_start and num < (source_start+range_val):
-                diff = num - source_start
-                new_num = dest_start + diff
-        new_nums.append(new_num)
+        for mapping in mappings:
+            for maps in mapping:
+                dest_start, source_start, range_val = maps
+                if new_num >= source_start and new_num < (source_start+range_val):
+                    diff = new_num - source_start
+                    new_num = dest_start + diff
+                    break  # break to stop the new_num value to be changed further in this round of mapping
 
-    return new_nums
+        min_num = min(min_num, new_num)
+    return min_num
+
+
+mappings = []
+num_ranges = []
+sum = 0
 
 with open('input.txt') as f:
     mapping = []
-    nums = []
     map_block = False
     for index, line in enumerate(f):
-        print(index)
         line = line.strip()
         if index == 0: # Always seeds first
             seeds = [int(i) for i in re.findall(r"(\d+)", line)]
             for i in range(0, len(seeds)-1, 2):
-                nums += list(range(seeds[i], seeds[i]+seeds[i+1]))
-            print(nums)
+                num_ranges.append((seeds[i], seeds[i]+seeds[i+1]))
+                sum += seeds[i+1]
         elif "map" in line:
             map_block = True
         elif map_block:
@@ -32,10 +37,12 @@ with open('input.txt') as f:
                 mapping.append([int(i) for i in re.findall(r"(\d+)", line)])
             else:
                 map_block = False
-                nums = remap_nums(nums, mapping)
-                mapping = []
-                print(nums)
+                mappings.append(mapping)
 
-nums = remap_nums(nums, mapping)
+                mapping = []
+    mappings.append(mapping)
+
+print(sum)
+nums = [remap_nums(num_range, mappings) for num_range in num_ranges]
 print(nums)
 print(min(nums))
