bc7fd6a136b8a292dc629f5c9e5e16bc37d23e49
andr-kun <ayang@ebi.ac.uk>
2023-12-07 07:20:31 +0000

    Simplified hand type scoring for puzzle 7 part 1

--- puzzle_7/solution.py
+++ puzzle_7/solution.py
@@ -6,28 +6,10 @@ card_strength = {j:i for i,j in enumerate("AKQJT98765432"[::-1])}
 def card_sort(card_bid):
     card, bid = card_bid
 
-    card_counts = list(Counter(card).values())
-    # Possible counts = [5], [4,1], [3,2], [3,1,1], [2,2,1], [2,1,1,1], [1,1,1,1,1]
-
-    hand_score = 0
-    if (freq_hand := max(card_counts)) > 1:
-        if freq_hand == 3:
-            if len(card_counts) == 2:  # [3,2]
-                hand_score = 4
-            else:  # [3,1,1]
-                hand_score = 3
-        elif freq_hand == 2:
-            if len(card_counts) == 3: # [2,2,1]
-                hand_score = 2
-            else:
-                hand_score = 1
-        else:  # [2,1,1,1]
-            hand_score = freq_hand + 1
-
+    card_counts = sorted(Counter(card).values(), reverse=True)
     card_score = [card_strength[c] for c in card]
 
-    print((card, hand_score, *card_score))
-    return (hand_score, *card_score)
+    return (card_counts, card_score)
 
 with open('input.txt') as f:
     cards_bids = []
